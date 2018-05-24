9389d363b4e621256797f9d0ee578c41ef176dbf
liu-nlper <jxliu.nlper@gmail.com>
2018-05-24 11:47:38 +0800

    update

--- TorchNN/layers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .crf import *
-from .bilstm import *
-from .bilstm_crf import *
--- TorchNN/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .util_general import *
-from .util_io import *
-from .util_data import *
-from .util_eval import *
-from .util_layer import *
